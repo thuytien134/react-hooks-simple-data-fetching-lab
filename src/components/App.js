cbb3a51ca82114fd6e5c91678e262436e91bf897
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-06-02 23:23:46 -0700

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React from "react"
+import {useState, useEffect} from "react"
+
+
+function App(){
+  const [randomDog,setRandomDog]= useState("")
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(()=>{
+    // setIsLoaded(true);
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then(data => {
+        setRandomDog(data.message)
+        setIsLoaded(true);
+    })
+    
+},[])
+
+if (!isLoaded) return <p>Loading...</p>;
+return (
+<>
+<img alt="A Random Dog" src={randomDog}/>
+</>
+)
+}
+
+export default App
